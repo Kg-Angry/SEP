a9f150d3a7d731a4c3dd750d84d7f5ab94244c48
JeremicAleks <jeremicaleksandar96@gmail.com>
2019-11-25 20:44:16 +0100

    zuul and koncentratorPlacanja spring initializr

---
+++ koncentratorPlacanja/src/main/java/com/sep/koncentratorPlacanja/KoncentratorPlacanjaApplication.java
@@ -0,0 +1,15 @@
+package com.sep.koncentratorPlacanja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class KoncentratorPlacanjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoncentratorPlacanjaApplication.class, args);
+	}
+
+}
---
+++ koncentratorPlacanja/src/test/java/com/sep/koncentratorPlacanja/KoncentratorPlacanjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sep.koncentratorPlacanja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoncentratorPlacanjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
