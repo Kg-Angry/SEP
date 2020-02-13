de1a8d3266ebc5e6ec2f74abe46fb1763b0a506c
JeremicAleks <jeremicaleksandar96@gmail.com>
2020-02-13 17:21:06 +0100

    update noviServis

--- NoviServis/src/main/java/com/sep/NoviServis/NoviServisApplication.java
+++ NoviServis/src/main/java/com/sep/NoviServis/NoviServisApplication.java
@@ -2,8 +2,10 @@ package com.sep.NoviServis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class NoviServisApplication {
